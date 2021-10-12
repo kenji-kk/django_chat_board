28aef704e08776d91d92ff2ab60b8f020f62dbdd
kenji <kenjix12x22@gmail.com>
2021-10-12 16:52:46 +0900

    ルーティング設定

--- app/urls.py
+++ app/urls.py
@@ -6,5 +6,6 @@ app_name = 'app'
 urlpatterns = [
     path('', auth_views.LoginView.as_view(template_name='app/login.html'), name='login'),
     path('signup/', views.signup, name='signup'),
-    path('userdetail/',views.userdetail, name='userdetail')
+    path('userdetail/', views.userdetail, name='userdetail')
+    path('logout/', auth_views.LogoutView.as_view(), name='logout')
 ]
