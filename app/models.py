dcb3419c140f22b97d1c683899d07daad85903d5
kenji <kenjix12x22@gmail.com>
2021-10-13 10:52:59 +0900

    ChatBoardモデル作成

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class ChatBoard(models.Model):
+
+  board_title = model.CharField(max_length=100)
+  user = models.OneToOneField(
+    get_user_model(),
+    on_delete=models.CASCADE
+  )
+  created_at = models.DateTimeField('登録日時', auto_now_add=True)
+
+  def __str__(self):
+    return self.title
