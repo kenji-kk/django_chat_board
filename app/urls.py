3dc34a3016d787fd14f4d11f7539b1c66fcc5b0e
kenji <kenjix12x22@gmail.com>
2021-10-13 16:29:33 +0900

    viewとテンプレート作成

--- app/urls.py
+++ app/urls.py
@@ -6,7 +6,7 @@ app_name = 'app'
 urlpatterns = [
     path('', auth_views.LoginView.as_view(template_name='app/login.html'), name='login'),
     path('signup/', views.signup, name='signup'),
-    path('userdetail/', views.userdetail, name='userdetail'),
+    path('userdetail/', views.user_detail, name='userdetail'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('createboard',views.createboard, name='createboard')
+    path('createboard',views.create_board, name='createboard')
 ]
