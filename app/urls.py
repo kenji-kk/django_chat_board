812c9ba6e0a23cee2b181bbcdc75c85874bc3c94
kenji <kenjix12x22@gmail.com>
2021-10-19 12:03:32 +0900

    ルーティング設定

--- app/urls.py
+++ app/urls.py
@@ -14,5 +14,6 @@ urlpatterns = [
     path('chatcontent/<int:pk>', views.chatcontent, name='chatcontent'),
     path('createcomment/<int:board_id>', views.createcomment, name='createcomment'),
     path('follow/<int:follower_id>', views.follow, name='follow'),
-    path('unfollow/<int:follower_id>', views.unfollow, name='unfollow')
+    path('unfollow/<int:follower_id>', views.unfollow, name='unfollow'),
+    path('followees/<int:user_id>', views.followees, name='followees')
 ]
